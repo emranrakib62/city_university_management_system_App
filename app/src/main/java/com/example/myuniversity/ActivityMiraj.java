80c974da9dfc1c22bbc34fb13ced6f9d7fdf0b34
Emranul Hoque <emranrakib01862@gmail.com>
2025-05-20 23:44:49 +0600

    updated

--- app/src/main/java/com/example/myuniversity/ActivityMiraj.java
+++ app/src/main/java/com/example/myuniversity/ActivityMiraj.java
@@ -15,10 +15,8 @@ public class ActivityMiraj extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_miraj);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+
     }
 }
